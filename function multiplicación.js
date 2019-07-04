80c4659f7a4c21071ef24986a8fa49c3bdd1dba4
Helena Coloma Díez <49346908+HelenaColoma@users.noreply.github.com>
2019-07-04 10:09:35 +0200

    Función para multiplicar dos parámetros

---
+++ function multiplicación.js
@@ -0,0 +1,11 @@
+function producto(param1, param2){
+    return param1*param2;
+}
+
+var resultado1=producto(8,9);
+var resultado2=producto(54,67);
+
+alert("El valor de la primera multiplicación es: "+resultado1);
+alert("El valor de la segunda multiplicación es: "+resultado2);
+
+//si queremos usar el resultado de la función más adelante (en una variable, por ejemplo) le ponemos un return para que el resultado quede grabado.
